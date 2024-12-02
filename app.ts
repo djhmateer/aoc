6fdd8ba90ee66d04c7df24ed93499fe9973be179
Dave Mateer <davemateer@gmail.com>
2024-12-02 14:19:14 +0000

    asfd

---
+++ app.ts
@@ -0,0 +1,33 @@
+let message: string = 'Hello, World9!';
+
+// gets a type error
+// let message: string = 2;
+
+console.log(typeof(message)); // string
+
+let foo
+foo = "Dave"
+console.log(typeof(foo)); // string
+
+let bar
+bar = 2
+console.log(typeof(bar)); // number
+
+let box: number = 100;
+console.log(typeof(box)); // number
+
+console.log(message);
+
+// i am a string
+// i am a number
+// in the end, i amg
+// ```+just me
+// i just want to be a string
+// this is a string
+// this is a number
+// that is a string
+// heenry is  a string
+// h
+// eenry is a number
+// hello i am henry and i want to be a good programmer. hello world
+
---
+++ out/app.js
@@ -0,0 +1,14 @@
+var message = 'Hello, World9!';
+// gets a type error
+// let message: string = 2;
+console.log(typeof (message)); // string
+var foo;
+foo = "Dave";
+console.log(typeof (foo)); // string
+var bar;
+bar = 2;
+console.log(typeof (bar)); // number
+var box = 100;
+console.log(typeof (box)); // number
+console.log(message);
+//# sourceMappingURL=app.js.map
