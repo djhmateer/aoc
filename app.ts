aa0b814944a59a51dddeb273d58483407733372c
Dave Mateer <davemateer@gmail.com>
2024-12-03 07:51:08 +0000

    auto

---
+++ app.ts
@@ -0,0 +1,41 @@
+let message = "good morning";
+let message2= "good afternoon";
+
+console.log(message);
+
+
+// gets a type error
+// let message: string = 2;
+
+// console.log(typeof(message)); // string
+
+// 
+// foo = "Dave"
+// console.log(typeof(foo)); // string
+
+// let bar
+// bar = 2
+// console.log(typeof(bar)); // number
+
+// let box: number = 100;
+// console.log(typeof(box)); // number
+
+// console.log(message);
+
+// i am a string
+// i am a number
+// in the end, i amg
+// ```+just me
+// i just want to be a string
+// this is a string
+// this is a number
+// that is a string
+// heenry is  a string
+// h
+// eenry is a number
+// hello i am henry and i want to be a good programmer. hello world
+
+// Dave is awesome
+//henry is awesome and i am a good footballer
+
+
