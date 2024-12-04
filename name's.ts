c814bff10cf3eb4d0cffa06b83959bf4c5e53363
Dave Mateer <davemateer@gmail.com>
2024-12-04 08:03:47 +0000

    auto

---
+++ name's.ts
@@ -0,0 +1,8 @@
+
+let message2: string = "hello henry";
+let message: string = "hello laurie";
+ let message3: string = "hello freddie";
+let message4: string = "hello dave";
+console.log(message);
+console.log(message2);
+console.log(message3);console.log(message4);
